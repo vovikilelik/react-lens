51a947e05ac88bd9320fc27b339c9c92d5dda692
svinokot <svinokot@svinokot>
2021-10-16 07:43:11 +0300

    1.6.0

--- src/test/ClassComponent.tsx
+++ src/test/ClassComponent.tsx
@@ -4,6 +4,7 @@ import { LensComponent } from "../react-lens";
 interface Props {
     className?: string;
 }
+
 export class ClassComponent extends LensComponent<number, Props> {
     public render() {
         const {lens} = this.props;
@@ -11,3 +12,23 @@ export class ClassComponent extends LensComponent<number, Props> {
         return <button onClick={ () => lens.set(value + 1) }>{ value }</button>
     }
 }
+
+const Counter = ({ value }) => {
+	return <div>{ value }</div>;
+};
+
+const Counter2 = ({ value, digit }) => {
+	return <div>{ `${value} ${digit}` }</div>
+};//`
+
+const Counter3 = ({ value, digit, type }) => {
+	const text = type ? `(${digit})` : digit;
+	return <div>{ `${value} ${text}` }</div>
+};//`
+
+const getCoveredDigits = (digits) => `(${digits})`;
+
+<Counter
+  value={value}
+  digits={getCoveredDigits(digits)}
+/>
